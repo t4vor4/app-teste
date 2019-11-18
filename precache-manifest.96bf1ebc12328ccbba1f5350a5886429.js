8c98c5bb1816f062f24fe2ee394e25b44c4b05e4
Fernando <fernando@agenciaeplus.com.br>
2019-11-18 07:06:24 -0300

    Updates

---
+++ precache-manifest.96bf1ebc12328ccbba1f5350a5886429.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "961347deb1c39caf717666870b5a5d98",
+    "url": "/app-teste/index.html"
+  },
+  {
+    "revision": "a0226ad5ff30d1bd9004",
+    "url": "/app-teste/static/css/main.cb87544a.chunk.css"
+  },
+  {
+    "revision": "9ebdc6fb9543686f072c",
+    "url": "/app-teste/static/js/2.5e00c6f0.chunk.js"
+  },
+  {
+    "revision": "a0226ad5ff30d1bd9004",
+    "url": "/app-teste/static/js/main.e56a6ff1.chunk.js"
+  },
+  {
+    "revision": "0312e93e92dd33a956aa",
+    "url": "/app-teste/static/js/runtime-main.897c0626.js"
+  }
+]);
